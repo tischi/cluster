39aa2c1b5836150006daf57425122f05ce3bae26
tischi <tischer@submaster1.(none)>
2015-11-18 17:40:51 +0100

    Some changes...

---
+++ cluster--fiji--20151118/test--cluster/jobs/job_4.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#BSUB -oo "test--cluster/log/job_4--out.txt"
+#BSUB -eo "test--cluster/log/job_4--err.txt"
+#BSUB -M 16000
+#BSUB -R select[mem>16000] -R rusage[mem=16000]
+ulimit -c 0
+The job started.echo "hostname:"
+hostname
+echo "number of cores:"
+nproc
+echo "total RAM:"
+touch "test--cluster/log/job_4--started.touch"
+xvfbfiji script some_file
